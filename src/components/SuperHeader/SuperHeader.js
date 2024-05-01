4f1da1eef9ebe940c02b3debfea39b004144460a
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-05-02 01:57:41 +0300

    Align elements in SuperHeader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,16 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  padding-block: 12px;
+  padding-inline: 32px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-inline-end: auto;
 `;
 
 const HelpLink = styled.a`
